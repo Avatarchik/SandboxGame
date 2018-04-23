378029330adc0f7303b7654bab2faa48ed9390dc
wanghaoran <wanghaoran1997@yahoo.com>
2018-04-23 21:51:00 +0800

    init

--- Assets/Scripts/Player/PlayerControlle.cs
+++ Assets/Scripts/Player/PlayerControlle.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class PlayerControlle : MonoBehaviour {
 
+    [SerializeField]
     private Transform m_Transform;
-    private GameObject m_BuildingPlan;
-    private Animator m_Animator;
+    [SerializeField]
+    private GameObject m_BuildingPlan;  //建造角色
+    private GameObject m_WoodenSpear;   //长矛角色
+    //private Animator m_Animator;
 
-    private bool isNormal = false; //true:空手 false:建造
+    private GameObject currentWeapon;   //当前武器
+    private GameObject targetWeapon;    //目标武器
+
+    //private bool isNormal = false; //true:空手 false:建造
 
 	void Start () {
         m_Transform = gameObject.GetComponent<Transform>();
-        m_BuildingPlan = m_Transform.Find("Character/Building Plan").gameObject;
-        m_Animator = m_BuildingPlan.GetComponent<Animator>();
-	}
+        m_BuildingPlan = m_Transform.Find("PersonCamera/Building Plan").gameObject;
+        m_WoodenSpear = m_Transform.Find("PersonCamera/Wooden Spear").gameObject;
+
+        m_WoodenSpear.SetActive(false);
+        //m_Animator = m_BuildingPlan.GetComponent<Animator>();
+        currentWeapon = m_BuildingPlan;
+        targetWeapon = null;
+    }
 	
 	void Update () {
+        //切换到建造
 		if(Input.GetKeyDown(KeyCode.M))
         {
-            Chaged();
+            targetWeapon = m_BuildingPlan;
+            Changed();
 
         }
-	}
+        //切换到长矛
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            targetWeapon = m_WoodenSpear;
+            Changed();
 
-    private void Chaged()
+        }
+    }
+
+    private void Changed()
     {
-        if(isNormal)
+        /*if(isNormal)
         {
             //抬起当前的物品.
             m_BuildingPlan.SetActive(true);
@@ -38,13 +58,21 @@ public class PlayerControlle : MonoBehaviour {
             m_Animator.SetTrigger("Holster");
             StartCoroutine("DelayTime");
             isNormal = true;
-        }
+        }*/
+
+        //放下当前武器
+        currentWeapon.GetComponent<Animator>().SetTrigger("Holster");
+        StartCoroutine("DelayTime");
+
     }
 
     IEnumerator DelayTime()
     {
         yield return new WaitForSeconds(1);
         //延迟1s,防止放下动画没有播放完就隐藏物体
-        m_BuildingPlan.SetActive(false);
+        currentWeapon.SetActive(false);
+        //抬起目标武器
+        targetWeapon.SetActive(true);
+        currentWeapon = targetWeapon;
     }
 }
--- README.md
+++ README.md
@@ -1,19 +1,43 @@
-# 沙盒Demo
-## 背包模块
+# **沙盒Demo**
+## **背包模块**
 ![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqksoca9atj20g105zgob.jpg)
-### 打开背包与合成面板
+* 打开背包与合成面板
+> 按 I 打开/关闭背包.
+
 ![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqksol54x0g20ws0ig0y9.jpg)
+* 拆分物品
+> 大量物品可以拆分，按右键半数拆分.
 
+![avatar](https://ayanamirei.oss-cn-hangzhou.aliyuncs.com/18-4-23/86706866.jpg)
+***
 
-## 合成模块
+## **合成模块**
 ![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqksoc33hij20ft07fq4b.jpg)
-### 合成物品与拆分
+* 合成物品
+> 把物品拖动到合成面板对应位置，点击合成，合成后剩余材料自动放回背包内.
+
 ![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqksolc4qlg20ws0igtit.jpg)
+***
 
-## 工具栏模块
+## **工具栏模块**
 ![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqksobywsnj20ft02faat.jpg)
-### 切换工具栏
-![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqksolu027g20ws0igwln.jpg)
+* 切换工具栏
+> 可以把背包中的物品拖到工具栏，然后按数字键1~8切换工具.
 
+![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqksolu027g20ws0igwln.jpg)
+***
 ## FPS模块
 ![avatar](https://ws1.sinaimg.cn/large/bcd41526ly1fqlj7b9z8rj20wf0if1eh.jpg)
+
+* 长矛模式
+> 按 M 切换成建造模式.（默认）
+
+![avatar](https://ayanamirei.oss-cn-hangzhou.aliyuncs.com/18-4-23/%E5%BB%BA%E9%80%A0%E6%A8%A1%E5%BC%8F.gif)
+
+* 长矛模式
+> 按 K 切换成长矛模式.
+
+![avatar](https://ayanamirei.oss-cn-hangzhou.aliyuncs.com/18-4-23/%E9%95%BF%E7%9F%9B%E6%A8%A1%E5%BC%8F.gif)
+
+***
+*最新更新于 2018.4.23*
